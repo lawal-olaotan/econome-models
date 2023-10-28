c42227fd8b81614a0abe2bcd76eea432b1d951b1
matat99 <mrmatat99@gmail.com>
2023-10-28 16:20:07 +0100

    adds relative time handling to the schedule_reminder function, adds variable comprehension to the get_subscription details

--- models/AI_Translator/conversation.py
+++ models/AI_Translator/conversation.py
@@ -2,11 +2,12 @@ import openai
 import os
 from functions import schedule_reminder, query_all_subscriptions, get_subscription_details
 from prompts import INIT_PROMPT
+from datetime import datetime, timedelta
 
 class Chatbot:
     # Shared configuration (constant for all instances)
     API_KEY = os.getenv("OPENAI_API_KEY")
-    FUNCTIONS = ["schedule_reminder:company:datetime", "query_all_subscriptions", "get_subscription_details", "unrecognised"]
+    FUNCTIONS = ["schedule_reminder:company:datetime", "query_all_subscriptions", "get_subscription_details:company", "unrecognised"]
     FUNCTION_MAP = {
         "schedule_reminder": schedule_reminder,
         "query_all_subscriptions": query_all_subscriptions,
@@ -42,6 +43,14 @@ class Chatbot:
                     return actual_response
                 except ValueError as e:
                     self.messages.append({"role": "assistant", "content": f"Error processing the request: {e}"})
+            if function_name == "get_subscription_details":
+                try: 
+                    _, company = response.split(":", 1)
+                    actual_response = get_subscription_details(company)
+                    self._reset_chat()
+                    return actual_response
+                except ValueError as e:
+                    self.messages.append({"role": "assistant", "content": f"Error processing the request: {e}"})
             else:
                 actual_response = self.FUNCTION_MAP[function_name]()
                 self._reset_chat()
@@ -64,14 +73,14 @@ if __name__ == "__main__":
     chatbot = Chatbot()
     
     # First interaction
-    query1 = "can you set a reminder?"
+    query1 = "How much am i paying for amazon?"
     print(f"User:", query1)
     print(f"Chatbot:", chatbot.get_response(query1))
 
-    # # Second interaction
-    query2 = "amazon a week from now"
-    print(f"User:", query2)
-    print(f"Chatbot:", chatbot.get_response(query2))
+    # # # Second interaction
+    # query2 = "amazon a week from now"
+    # print(f"User:", query2)
+    # print(f"Chatbot:", chatbot.get_response(query2))
 
     # # # Third interaction
     # query3 = "yes"
--- models/AI_Translator/functions.py
+++ models/AI_Translator/functions.py
@@ -1,13 +1,28 @@
+from datetime import datetime, timedelta
+
 #functions go here
 
 def schedule_reminder(company, time):
-	print(f"Reminder scheduled for {company} on {time}")
-	return f"Reminder scheduled for {company} on {time}"
+    # Check if time is a relative time
+    if "+" in time or "-" in time:
+        try:
+            # Convert relative time to actual date
+            delta_days = int(time)
+            reminder_date = datetime.now() + timedelta(days=delta_days)
+            formatted_date = reminder_date.strftime('%d/%m/%Y')
+        except ValueError as e:
+            return f"Error processing the request: {e}"
+    else:
+        formatted_date = time 
+
+    print(f"Reminder scheduled for {company} on {formatted_date}")
+    return f"Reminder scheduled for {company} on {formatted_date}"
+
 
 def query_all_subscriptions():
 	print("hello all")
 	return "you pay $4000 a month"
 
-def get_subscription_details():
-	print("detailed hello")
+def get_subscription_details(company):
+	print(company)
 	return "you pay $300 a month for Amazon Prime"
